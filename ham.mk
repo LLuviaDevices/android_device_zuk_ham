6955d5f4ebfd6bf42418fa527493bce7a064c1c8
Srijith Bharadwaj <srijithdamerla2001@gmail.com>
2019-10-27 12:34:28 +0000

    ham:build soundtrigger hal
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- ham.mk
+++ ham.mk
@@ -69,6 +69,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.0-service
     audio.a2dp.default \
     audio_amplifier.msm8974 \
     audio_policy.msm8974 \
